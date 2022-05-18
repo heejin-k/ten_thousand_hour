d7e89425792bcb5105132131a3631f0cdcc78434
heejin <khj16355@naver.com>
2022-05-18 23:52:56 +0900

    js 추가

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,27 @@
-// js file
+
+const btn = document.querySelector('.howmany');
+const modal = document.querySelector('#modal');
+const result = document.querySelector('.wrap-result');
+const go = document.querySelector('#gotrain')
+
+
+const job = document.querySelector('#job');
+const inputjob = document.querySelector('#jobname');
+const hour = document.querySelector('#hour');
+const inputhour = document.querySelector('#trainhour');
+const modalbtn = document.querySelector('.modalbtn')
+
+btn.addEventListener('click', () => {
+    result.classList.add('show');
+    job.textContent = inputjob.value;
+    hour.textContent = (10000 / (inputhour.value));
+})
+
+go.addEventListener('click', () => {
+    modal.classList.toggle('show');
+})
+
+modalbtn.addEventListener('click', () => {
+    modal.classList.toggle('show');
+})
+
